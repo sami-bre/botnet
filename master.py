51e0e093cf9406dc91cb04cf311898a3dc6193e2
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-09-25 17:13:41 +0300

    I added the master.py file. This file is supposed to be the controller script for the botnet. It currently has comments on what to implement.

---
+++ master.py
@@ -0,0 +1,9 @@
+# this will be the master script that allows me to controll
+# zombies.
+
+# some of the features to be implemented are:
+#   looking at the execution history of a certain zombie (the table of the zombie, e.g. table z3)
+#   looking at the waiting command of a certian zombie (from the 'zombies' table)
+#   modifying the waiting command of a certain zombie
+
+# I assume this script will run on the terminal and it will have a main loop
